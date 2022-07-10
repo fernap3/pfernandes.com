a57e796383fc2b0cf09a929e106c62eaea054cb4
Peter Fernandes <supersonicandtails@gmail.com>
2022-07-10 15:04:04 -0700

    Basic routing and translation infra up

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const path = require("path");
+const fs = require("fs").promises;
+const translations = require("./translations.json");
 
 const STRIPE_KEY = process.env.STRIPE_KEY;
 
@@ -18,16 +20,66 @@ const stripe = require("stripe")(STRIPE_KEY);
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { response } = require("express");
 
 app.use(bodyParser.json());
 
-app.get("/", (request, response) =>
+app.get("/*", async (req, res) =>
 {
-	response.status(200).sendFile(path.resolve(".", "static", "index.html"));
+	const pageLang = req.path.startsWith("/jp") ? "jp" : "en";
+	let urlPath = req.path.replace("/jp", "");
+	if (urlPath === "")
+		urlPath = "/";
+
+	if (urlPath === "/")
+		urlPath = "/index.html";
+
+	let urlPathParts = urlPath.split("/");
+	let pathOnDisk = path.resolve(".", "static", ...urlPathParts);
+
+	const topLevelPages = new Set(["incline", "resume"]);
+
+	if (topLevelPages.has(urlPathParts.at(-1)))
+	{
+		urlPath = urlPath + ".html";
+		urlPathParts[urlPathParts.length - 1] = urlPathParts[urlPathParts.length - 1] + ".html";
+		pathOnDisk = pathOnDisk + ".html";
+	}
+	
+	try
+	{
+		await fs.stat(pathOnDisk);
+	}
+	catch(e)
+	{
+		res.status(404).send("Resource not found");
+		return;
+	}
+
+	if (urlPath.endsWith(".html"))
+	{
+		res.status(200).send(await translateHtml(pathOnDisk, pageLang));
+		return;
+	}
+
+	res.status(200).sendFile(pathOnDisk);
 });
 
-app.use(express.static("static"));
+async function translateHtml(fullPath, lang)
+{
+	const fileText = await fs.readFile(fullPath, { encoding: "utf8" });
+	const translationIndex = lang === "jp" ? 1 : 0;
 
+	return fileText.replace(/\{\{(.+?)\}\}/g, (matchValue, langKey) => {
+		const keyParts = langKey.split(".").slice(1); // Get rid of the "lang." prefix from the key
+		const lang = translations["index.html"];
+
+		if (lang[keyParts[0]] == null)
+			return matchValue;
+
+		return lang[keyParts[0]][translationIndex] ?? matchValue;
+	});
+}
 
 const PORT = process.env.PORT || 5001;
 
