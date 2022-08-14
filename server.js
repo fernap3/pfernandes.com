eedceac72715b8a2002a39c4bac1c251d56e7ba5
Peter Fernandes <supersonicandtails@gmail.com>
2022-08-14 15:38:49 -0700

    Turn on gzip compression

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const fs = require("fs").promises;
 const translations = require("./translations.json");
 const countryCodes = require("country-code-lookup");
+const compression = require("compression");
 
 const TEST_MODE = process.env.LIVE_MODE == null || process.env.LIVE_MODE == "";
 
@@ -72,6 +73,7 @@ const bodyParser = require("body-parser");
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 app.use(bodyParser.json());
+app.use(compression());
 app.enable("trust proxy");
 
 app.get(/\/(jp\/)?unsubscribe/, async (req, res) =>
