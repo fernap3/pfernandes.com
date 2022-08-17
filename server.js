947fb9d82ae2dff2b503cac156ee1824fc81f0d2
Peter Fernandes <supersonicandtails@gmail.com>
2022-08-17 15:16:45 -0700

    Add helper to evaluate template text from a context variable

--- server.js
+++ server.js
@@ -74,6 +74,10 @@ const bodyParser = require("body-parser");
 
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
+const hb = handlebars.create({
+	layout: false,
+	extname: "hbs",
+});
 
 app.engine(".hbs", handlebars.engine({
 	layout: false,
@@ -201,6 +205,9 @@ async function handleResourceGet(req, res)
 			helpers: {
 				lang: (langId) => {
 					return pageTranslations[langId];
+				},
+				evalAsTemplate: (templateText, context) => {
+					return hb.handlebars.compile(templateText)(context);
 				}
 			}
 		});
